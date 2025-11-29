9af4426da81848b4b6734632e025a76682508ae1
DhekraBalbouli20 <dhekrabalbouli11@gmail.com>
2025-11-29 19:18:26 +0100

    Ajouter la classe Tache pour représenter une tâche côté serveur et permettre sa gestion

---
+++ server/gestionnaire_taches.py
@@ -0,0 +1,7 @@
+class Tache:
+    def __init__(self, id, titre, description, auteur, statut="TODO"):
+        self.id = id
+        self.titre = titre
+        self.description = description
+        self.statut = statut
+        self.auteur = auteur
