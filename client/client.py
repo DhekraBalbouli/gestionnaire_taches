a4b706403dbeb0320a9b5f30cd26e3fd3c52d529
DhekraBalbouli20 <dhekrabalbouli11@gmail.com>
2025-11-29 21:15:05 +0100

    Ajout du menu interactif côté client pour gérer les tâches (add, list, del, statut)

--- client/client.py
+++ client/client.py
@@ -13,4 +13,31 @@ class ClientTaches:
         response = self.client_socket.recv(4096).decode()
         return response
 
-
+def menu():
+    client = ClientTaches()
+    while True:
+        print("\n1. Ajouter tâche\n2. Lister tâches\n3. Supprimer tâche\n4. Changer statut\n5. Quitter")
+        choix = input("Choix : ")
+        if choix == "1":
+            tache = {
+                "action": "add",
+                "id": input("ID : "),
+                "titre": input("Titre : "),
+                "description": input("Description : "),
+                "auteur": input("Auteur : ")
+            }
+            print(client.envoyer_requete(tache))
+        elif choix == "2":
+            print(client.envoyer_requete({"action": "list"}))
+        elif choix == "3":
+            id_del = int(input("ID à supprimer : "))
+            print(client.envoyer_requete({"action": "del", "id": id_del}))
+        elif choix == "4":
+            id_stat = input("ID : ")
+            nouveau = input("Nouveau statut (TODO/DOING/DONE) : ")
+            print(client.envoyer_requete({"action": "statut", "id": id_stat, "nouveau_statut": nouveau}))
+        elif choix == "5":
+            break
+        
+if __name__ == "__main__":
+    menu()
