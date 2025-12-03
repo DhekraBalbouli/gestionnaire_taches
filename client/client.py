cdb3cca4e4d27bccd6a22e703c5a7ae3a8487c4e
DhekraBalbouli20 <dhekrabalbouli11@gmail.com>
2025-12-03 17:39:31 +0100

    Modification du client.py pour fonctionner dans Docker (host.docker.internal)

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ import socket
 import json
 
 class ClientTaches:
-    def __init__(self, host='127.0.0.1', port=5000):
+    def __init__(self, host='host.docker.internal', port=5000):
         self.host = host
         self.port = port
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
